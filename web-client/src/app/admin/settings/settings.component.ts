055c76513c1455df5557843fee211e2c4dd048c4
ddzemesh <demesh.dima.dd@gmail.com>
2021-03-18 20:50:36 +0300

    add calendar for admin portal

--- web-client/src/app/admin/settings/settings.component.ts
+++ web-client/src/app/admin/settings/settings.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {of} from "rxjs";
+import {MatCalendar} from "@angular/material/datepicker";
+
+class Holiday {
+  constructor(public id?: number, public startDate?: Date, public transferDate?: Date) {
+  }
+}
 
 @Component({
   selector: 'app-settings',
@@ -15,6 +21,14 @@ export class SettingsComponent implements OnInit {
       ];
   selectedDate: any;
   selectedCalendar: any;
+  holidays = [
+      new Holiday(1, new Date(), new Date('2021-03-17'))
+  ];
+  dates = new Set();
+  transfers = new Set();
+  holiday = new Holiday();
+
+  @ViewChild('calendar') calendar: MatCalendar<Date>;
 
   constructor() { }
 
@@ -33,21 +47,71 @@ export class SettingsComponent implements OnInit {
   }
 
   onSelect($event: any) {
-    console.log($event);
+    if (!this.holiday.startDate) {
+      this.holiday.startDate = $event;
+    } else if (!this.holiday.transferDate) {
+      this.holiday.transferDate = $event;
+    } else {
+      this.holiday.startDate = $event;
+      this.holiday.transferDate = null;
+    }
+    this.calendar.updateTodaysDate();
   }
 
   dateClass() {
     return (date: Date) => {
-      if (date.getDate() === 1) {
-        return 'special-date';
-      } else {
-        return;
+      if (this.holiday && this.holiday.startDate && date.toDateString() == this.holiday.startDate.toDateString()) {
+        return 'selected-start-date';
+      } else if (this.holiday && this.holiday.transferDate && date.toDateString() == this.holiday.transferDate.toDateString()) {
+        return 'selected-shift-date';
+      } else if (this.dates && this.dates.has(date.toDateString())) {
+        return 'start-date';
+      } else if (this.transfers && this.transfers.has(date.toDateString())) {
+        return 'shift-date';
       }
+      return '';
     };
+  }
+
+  loadHolidays() {
+    of([
+      new Holiday(1, new Date(), new Date('2021-03-17'))
+    ])
+        .subscribe(holidays => {
+          this.holidays = holidays;
+          this.splitDates();
+        })
+  }
+
+  private splitDates() {
+    this.holidays.forEach(holiday => {
+      this.dates.add(holiday.startDate.toDateString());
+      if (holiday.transferDate) {
+        this.transfers.add(holiday.transferDate.toDateString());
+      }
+    })
+    this.calendar.updateTodaysDate();
   }
 
   selectCalendar($event) {
     this.selectedCalendar = $event.option.value;
+    this.loadHolidays();
+  }
 
+  deleteHoliday(holiday) {
+    of(holiday)
+        .subscribe(() => {
+          this.holidays.splice(this.holidays.indexOf(holiday), 1);
+          this.splitDates();
+        });
+  }
+
+  addHoliday() {
+    of(this.holiday)
+        .subscribe(holiday => {
+          this.holidays.push(holiday);
+          this.holiday = new Holiday();
+          this.splitDates();
+        })
   }
 }
