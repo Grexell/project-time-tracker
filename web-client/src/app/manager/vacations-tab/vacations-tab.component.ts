011b2e945519ca0fdc4740d4676e93ae07102108
ddemesh <demesh_dima@mail.ru>
2021-04-12 21:44:34 +0300

    add ui for manager's vacation tab

--- web-client/src/app/manager/vacations-tab/vacations-tab.component.ts
+++ web-client/src/app/manager/vacations-tab/vacations-tab.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./vacations-tab.component.scss']
 })
 export class VacationsTabComponent implements OnInit {
+  vacationsByUser = [];
+  vacationsByProject = [];
 
   constructor() { }
 
