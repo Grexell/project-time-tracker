8346c1ff9f403d2f637d8d85b13e0bca2399efb0
ddzemesh <demesh.dima.dd@gmail.com>
2021-05-12 22:40:25 +0300

    add holidays data

--- database/src/sql/data.sql
+++ database/src/sql/data.sql
@@ -1,6 +1,20 @@
 insert into time_tracker.role(name) values ('admin'), ('manager'), ('user');
 insert into time_tracker.calendar(locale) values ('be_BY');
 
+insert into holiday(date, calendar_id) values ('2021-01-01', 1);
+insert into holiday(date, calendar_id) values ('2021-01-07', 1);
+insert into holiday(date, calendar_id, transfer_date) values ('2021-01-08', 1, '2021-01-16');
+insert into holiday(date, calendar_id) values ('2021-03-08', 1);
+insert into holiday(date, calendar_id) values ('2021-05-01', 1);
+insert into holiday(date, calendar_id) values ('2021-05-02', 1);
+insert into holiday(date, calendar_id) values ('2021-05-09', 1);
+insert into holiday(date, calendar_id, transfer_date) values ('2021-05-10', 1, '2021-05-15');
+insert into holiday(date, calendar_id) values ('2021-05-11', 1);
+insert into holiday(date, calendar_id) values ('2021-07-03', 1);
+insert into holiday(date, calendar_id) values ('2021-11-07', 1);
+insert into holiday(date, calendar_id) values ('2021-12-25', 1);
+insert into holiday(date, calendar_id) values ('2021-12-31', 1);
+
 insert into time_tracker.user(first_name, second_name, email, password, role_id, calendar_id)
 values ('admin', 'admin', 'admin@company.com', '$2a$10$EFEJzuZA3DLrNeB8bIWv9eviqKs05bBkKe3ZQfBn8itQukd5jDFH2', 1, 1);-- password = admin
 
